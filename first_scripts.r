820e170c7338e3e1867a0a89ccb0d937952bf33c
Chancellor_Woolsey <chancellorwoolsey@gmail.com>
2022-07-14 19:39:26 -0500

    Create first_scripts.r
    Basics from chapter One of "Programming Skills For Data Science"  By Michael Freeman and John Ross

---
+++ first_scripts.r
@@ -0,0 +1,21 @@
+#basic variable assignment
+minutes_in_a_year <- 356*24*60
+print(minutes_in_a_year)
+
+#basic string assignment
+name<- "Octavia Butler"
+
+#basic boolean assignment
+basic_boolTrue <- TRUE
+basic_boolFalse <- FALSE
+
+#logical comparisons
+
+num_guitar_strings <- 6
+num_mandolin_strings <- 8
+
+num_guitar_strings > num_mandolin_strings
+
+num_guitar_strings != num_mandolin_strings
+
+"mandolin" > "guitar"
