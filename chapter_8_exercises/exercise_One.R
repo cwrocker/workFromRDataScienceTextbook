dd078746cb643b1abf921dfae0ae060256f573b0
Chancellor_Woolsey <chancellorwoolsey@gmail.com>
2022-07-16 14:34:39 -0500

    create Exercise_One.R

---
+++ chapter_8_exercises/exercise_One.R
@@ -0,0 +1,64 @@
+# Exercise 1: creating and accessing lists
+
+# Create a vector `my_breakfast` of everything you ate for breakfast
+my_breakfast <- c("Pancakes", "Bacon", "Eggs")
+
+# Create a vector `my_lunch` of everything you ate (or will eat) for lunch
+my_lunch <- c("Sandwhich", "Thin_Mints", "Arnie_Palmer")
+
+# Create a list `meals` that has contains your breakfast and lunch
+meals<- list(
+  breakfast = my_breakfast,
+  lunch = my_lunch
+)
+
+# Add a "dinner" element to your `meals` list that has what you plan to eat 
+# for dinner
+meals$dinner <- c('Burgers', "Fries", 'Milkshake')
+
+# Use dollar notation to extract your `dinner` element from your list
+# and save it in a vector called 'dinner'
+dinner <- c(meals$dinner)
+is.vector(dinner)
+
+# Use double-bracket notation to extract your `lunch` element from your list
+# and save it in your list as the element at index 5 (no reason beyond practice)
+meals[[5]] <- meals[['lunch']]
+
+# Use single-bracket notation to extract your breakfast and lunch from your list
+# and save them to a list called `early_meals`
+early_meals <- list(
+  breakfast = meals['breakfast'],
+  lunch = meals['lunch']
+)
+
+early_meals$breakfast
+### Challenge ###
+
+# Create a list that has the number of items you ate for each meal
+# Hint: use the `lappy()` function to apply the `length()` function to each item
+numberofItemsEaten <- list(
+  breakfast = length(my_breakfast),
+  lunch = length(my_lunch),
+  dinner = length(dinner)
+  
+)
+
+numberofItemsEaten$breakfast
+numberofItemsEaten$lunch
+numberofItemsEaten$dinner
+
+# Write a function `add_pizza` that adds pizza to a given meal vector, and
+# returns the pizza-fied vector
+add_pizza <- function (mealVector)
+{
+  endofVector<-length(mealVector)
+  mealVector[endofVector+1]<- 'pizza'
+  mealVector
+}
+
+
+# Create a vector `better_meals` that is all your meals, but with pizza!
+
+better_meals <- add_pizza(my_lunch)
+
