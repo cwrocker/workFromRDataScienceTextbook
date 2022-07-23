10dd0489f0acbc2b885c7869b1520d9b3d773f63
Chancellor_Woolsey <chancellorwoolsey@gmail.com>
2022-07-23 00:12:09 -0500

    chapter_11_is_done

--- chapter_11_exercises/exercise_Seven.R
+++ chapter_11_exercises/exercise_Seven.R
@@ -30,26 +30,76 @@ topAssistsPerGame <- nbaStats %>%
   mutate(assistsPerGame = AST / G) %>%
   arrange(-assistsPerGame)
 
-View(nbaStats)
+View(topAssistsPerGame)
 # Create a data frame called `good_offense` of teams that scored more than 
 # 8700 points (PTS) in the season
+good_offense <- topAssistsPerGame %>%
+  filter(PTS >8700)
 
-
+View(good_offense)
 # Create a data frame called `good_defense` of teams that had more than 
 # 470 blocks (BLK)
+good_defense <- topAssistsPerGame %>%
+  filter(BLK >470)
 
+View(good_defense)
 
 # Create a data frame called `offense_stats` that only shows offensive 
 # rebounds (ORB), field-goal % (FG.), and assists (AST) along with the team name.
+offense_stats <- topAssistsPerGame %>%
+  select(
+    Team,
+    ORB,
+    FG.,
+    AST
+  )
 
+View(offense_stats)
 
 # Create a data frame called `defense_stats` that only shows defensive 
 # rebounds (DRB), steals (STL), and blocks (BLK) along with the team name.
+defense_stats <- topAssistsPerGame %>%
+  select(
+    Team,
+    DRB,
+    STL,
+    BLK
+  )
 
+View(defense_stats)
 
 # Create a function called `better_shooters` that takes in two teams and returns
 # a data frame of the team with the better field-goal percentage. Include the 
 # team name, field-goal percentage, and total points in your resulting data frame
-
-
+better_shooters <- function(team1, team2)
+{
+  team1FG <- nbaStats %>%
+    filter(Team == team1) %>%
+    select(FG.)
+  
+  team2FG <- nbaStats %>%
+    filter(Team == team2) %>%
+    select(FG.)
+  
+  if(team1FG > team2FG)
+  {
+    team1Stats <- nbaStats %>%
+      filter(Team == team1) %>%
+      select(Team, FG., PTS)
+    
+    team1Stats
+    
+  } else
+  {
+    team2Stats <- nbaStats %>%
+      filter(Team == team2) %>%
+      select(Team, FG., PTS)
+    
+    team2Stats
+  }
+  
+}
 # Call the function on two teams to compare them (remember the `*` if needed)
+better_shooters('Golden State Warriors*', 'Atlanta Hawks*')
+
+better_shooters('Atlanta Hawks*', 'Golden State Warriors*')
